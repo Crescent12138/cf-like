d8861e21699823e3301399ad48172becaf48c342
Crescent12138 <2986325137@qq.com>
2025-04-09 22:56:16 +0800

    add map

--- src/data/cfProblems.h
+++ src/data/cfProblems.h
@@ -1,24 +1,66 @@
 #pragma once
-#include "proto/feed.pb.h"
-#include "com/dataType.h"
 #include "client/cfClient.h"
+#include "com/dataType.h"
+#include "proto/feed.pb.h"
+#include "util/jsonUtils.h"
+
 namespace suggest {
-    class CfProblems : public DictData<std::string, Feed>{
-        public:
-        CfProblems(){
-            CfClient client{};
-            // parse TODO
+class CfProblems : public DictData<std::string, Feed> {
+public:
+    CfProblems() {
+        CfClient    client{};
+        std::string status;
+        int         code = 0;
+        auto       &doc  = client.doc;
+        if (doc.HasMember("status") && doc["status"].IsString()) {
+            std::string status = doc["status"].GetString();
+            if (status != std::string("OK")) {
+                LOG(ERROR) << "status :" << doc["status"].GetString() << "\n";
+                return;
+            }
+            if (doc.HasMember("result") && doc["result"].HasMember("problems") && doc["result"]["problems"].IsArray()) {
+                auto &problems = doc["result"]["problems"];
+                if (problems.Size() > 0) {
+                    LOG(INFO) << "size :" << problems.Size() << "\n";
+                }
+                for (auto i = 0; i < problems.Size(); i++) {
+                    Feed        feed;
+                    std::string name = problems[i]["name"].GetString();
+                    feed.set_title(name);
+                }
+                // auto * feed = ctx->resp_->mutable_feedlist()->Add();
+                // // feed->mutable_title() = ;
+                // feed->set_title(problems[0]["name"].GetString());
+            }
         }
-    };
-    class CfProblemHandler {
-        public:
-        static void load(){
-            std::shared_ptr<CfProblems> pk_ptr = std::make_shared<CfProblems>();
-            list_ptr = pk_ptr;
-        }
-        static std::shared_ptr<CfProblems> list_ptr ;
-        
-    };
-    static CfProblemHandler cfProblemHandler;
-    std::shared_ptr<CfProblems> CfProblemHandler::list_ptr = nullptr;
-}
+        // code = utils::get_rapidjson_string(client.doc, "status", status);
+        // if(code != 0 || status != std::string("OK")){
+        //     LOG(ERROR) << "code:" <<code <<";status:"<<status;
+        //     return ;
+        // } else {
+        //     // auto  result = client.doc["result"];
+        //     // code = utils::get_rapidjson_Object(client.doc, "result", result);
+        //     if(code != 0){
+        //         LOG(ERROR) << "code:" <<code;
+        //         return ;
+        //     }else{
+
+        //     }
+        // }
+        // parse TODO
+    }
+};
+
+class CfProblemHandler {
+public:
+    static void load() {
+        std::shared_ptr<CfProblems> pk_ptr = std::make_shared<CfProblems>();
+        list_ptr                           = pk_ptr;
+    }
+
+    static std::shared_ptr<CfProblems> list_ptr;
+};
+
+static CfProblemHandler     cfProblemHandler;
+std::shared_ptr<CfProblems> CfProblemHandler::list_ptr = nullptr;
+}  // namespace suggest
--- src/util/jsonUtils.h
+++ src/util/jsonUtils.h
@@ -1,29 +1,31 @@
 #pragma once
+#include <stdlib.h>
+#include <cstdlib>
 #include <json2pb/rapidjson.h>
 
 namespace utils {
 
-static int get_rapidjson_string(const butil::rapidjson::Document& js_in, const std::string& key, std::string& value) {
+// static int get_rapidjson_string(butil::rapidjson::Document& js_in,  std::string key, std::string& value) {
 
-    if (!js_in.HasMember(key) && js_in[key].IsString()) {
-        value = js_in[key].GetString();
-        return 0;
-    } else {
-        return -1;
-    }
-}
+//     if (!js_in.HasMember(key) && js_in[key].IsString()) {
+//         value = js_in[key].GetString();
+//         return 0;
+//     } else {
+//         return -1;
+//     }
+// }
 
-static int get_rapidjson_int(const butil::rapidjson::Document& js_in, const std::string& key, int& value) {
-    if (!js_in.HasMember(key) && js_in[key].IsInt()) {
-        value = js_in[key].GetInt();
-        return 0;
-    } else {
-        return -1;
-    }
-}
-// static int get_rapidjson_Object(const butil::rapidjson::Document& js_in, const std::string& key, int& value) {
+// static int get_rapidjson_int(butil::rapidjson::Document& js_in,  std::string key, int& value) {
 //     if (!js_in.HasMember(key) && js_in[key].IsInt()) {
 //         value = js_in[key].GetInt();
+//         return 0;
+//     } else {
+//         return -1;
+//     }
+// }
+// static int get_rapidjson_Object(butil::rapidjson::Document& js_in, std::string key, butil::rapidjson::GenericValue<butil::rapidjson::UTF8<> >& value) {
+//     if (!js_in.HasMember(key) && js_in[key].IsObject()) {
+//         value = js_in[key].GetObejct();
 //         return 0;
 //     } else {
 //         return -1;
